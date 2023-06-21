8601a1a6738838e0241877ea293fb47953fd795c
wcl <will.lasala@onespan.com>
2023-06-21 13:02:59 -0400

    fix: added cache for gravatar image url

--- lib/gravatar_image_view.dart
+++ lib/gravatar_image_view.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gravatar/flutter_gravatar.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class GravatarImageView extends StatelessWidget {
   final String email;
   final double? height;
   final double? width;
   final String defaultFriendAvatarImageStr = "assets/images/avatars_3d_avatar_28.png";
+  static final Map<String, String> _GravatarImgUrlCache = {};
   const GravatarImageView({Key? key, required this.email, this.width, this.height = 0.0}) : super(key: key);
 
   Future<String> getFriendAvatarImage() async {
     return Future.microtask(() async {
-      // debugPrint('Getting gravatar data for $email');
-      if (email != "") {
+      debugPrint('Getting gravatar data for $email');
+      if (email.isNotEmpty) {
+        if (_GravatarImgUrlCache.containsKey(email)) {
+          return _GravatarImgUrlCache[email] as String;
+        }
         String gravatarImgUrl =
             Gravatar(email).imageUrl(size: width?.toInt() ?? 120, defaultImage: defaultFriendAvatarImageStr);
-        // debugPrint('found gravatar image url for $email, $gravatarImgUrl');
+        debugPrint('found gravatar image url for $email, $gravatarImgUrl');
+        _GravatarImgUrlCache[email] = gravatarImgUrl;
         return gravatarImgUrl;
       }
       return defaultFriendAvatarImageStr;
@@ -37,7 +43,7 @@ class GravatarImageView extends StatelessWidget {
   Widget _fadeInWidget(imgUrlStr) {
     return FadeInImage(
         placeholder: AssetImage(defaultFriendAvatarImageStr),
-        image: NetworkImage(imgUrlStr),
+        image: CachedNetworkImageProvider(imgUrlStr),
         imageErrorBuilder: (context, error, stackTrace) {
           return Image.asset(defaultFriendAvatarImageStr,
               width: width != 0.0 ? width : null, height: height != 0.0 ? height : null);
