6506a8829a6a0397e1dcfdaf614e562b65319cd2
Will LaSala <mrtaz28@gmail.com>
2026-05-31 10:46:32 -0400

    fix(android): dynamically align KotlinCompile jvmTarget with Java targetCompatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,26 @@ subprojects {
     }
 }
 
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        val javaVersion = android?.compileOptions?.targetCompatibility
+        if (javaVersion != null) {
+            val targetStr = javaVersion.toString()
+            val jvmTargetValue = when (targetStr) {
+                "1.8" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+                "11" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+                "17" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                "21" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+                else -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+            }
+            compilerOptions {
+                jvmTarget.set(jvmTargetValue)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
